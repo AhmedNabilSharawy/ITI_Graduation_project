86ba300a8fae6e34e9bf566474556c8d9e4fa11e
AhmedNabilSharawy <ahmednabilsharawy@gmail.com>
2023-06-13 15:03:25 +0300

    update commands.sh to get token of SA

--- Terraform/commands.sh
+++ Terraform/commands.sh
@@ -1,3 +1,5 @@
 kubectl exec -n tools -it jenkins-745954bdbf-hqzzq -- cat /var/jenkins_home/secrets/initialAdminPassword
 
 kubectl exec -it nexus-7d49586c8b-29lfl -n tools cat /nexus-data/admin.password
+
+kubectl get secret $(kubectl get serviceaccount jenkins -o jsonpath='{.secrets[0].name}') -o jsonpath='{.data.token}' | base64 --decode
